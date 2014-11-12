728d7460fe828ae4080a0b4a50e90e6991f471f5
Joerg Boeselt <joerg@brightcookie.com.au>
2014-11-12 13:55:19 +1030

    For node-babies, like myself.

---
+++ readme.md
@@ -0,0 +1,24 @@
+# xAPIWrapper + Node + Jasmine boilerplate
+
+* https://github.com/adlnet/xAPIWrapper
+* http://jasmine.github.io
+
+Node testing using Jasmine's [node.js](http://jasmine.github.io/2.0/node.html)
+
+## Requirements
+
+Jasmine >= 2.0
+xAPIWrapper namespaces into the window object:
+
+ * `jsdom` [module](https://github.com/tmpvar/jsdom)
+ * [howto](https://github.com/tmpvar/jsdom#creating-a-browser-like-window-object)
+
+
+```
+$ npm install -g jasmine
+$ cd ~/project_root
+$ npm install jsdom #note: install -g hangs on *NIX due to some weird perm issues
+$ git clone https://github.com/adlnet/xAPIWrapper.git
+$ jasmine init
+$ jasmine
+```
---
+++ spec/xapiwrapper.spec.js
@@ -0,0 +1,47 @@
+/**
+ * Setup:
+ * $ npm install -g jasmine
+ * $ cd ~/project_root
+ * $ npm install jsdom # install -g fails on *NIX
+ * $ git clone https://github.com/adlnet/xAPIWrapper.git
+ * $ jasmine init
+ * $ jasmine
+ */
+'use strict'
+
+/**
+ * xAPIWrapper requires window object!
+ * @see https://github.com/tmpvar/jsdom#creating-a-browser-like-window-object
+ */
+var jsdom = require('jsdom').jsdom;
+global.document = jsdom('Jasmine: Base xAPIWrapper test');//faky fake!
+global.window = document.parentWindow;
+
+/**
+ * include library files (is not a node module)
+ */
+var vm = require('vm');
+var fs = require('fs');
+vm.runInThisContext(fs.readFileSync('xAPIWrapper/xapiwrapper.min.js'));
+global.ADL = window.ADL;
+
+
+/**
+ * Testing ADL.XAPIWrapper
+ */
+
+var defaults = {};
+
+describe('ADL.XAPIWrapper', function() {
+
+    it('ADL instance should be defined.', function() {
+        expect(ADL).toBeDefined();
+    });
+    it('ADL.XAPIWrapper should be defined.', function() {
+        expect(ADL.XAPIWrapper).toBeDefined();
+    });
+    it('ADL.XAPIWrapper.base should be a uri (/http/).', function() {
+        expect(ADL.XAPIWrapper.base).toMatch(/http/);
+    });
+    
+});
